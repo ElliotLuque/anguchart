e4c70d3b635241ca0745ce94af58cde01e17ce30
ElliotLuque <elliotluque@gmail.com>
2022-12-01 19:48:30 +0100

    feat: chart-builder component

---
+++ src/app/components/unrouted/chart-builder/chart-builder.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ChartBuilderComponent } from './chart-builder.component';
+
+describe('ChartBuilderComponent', () => {
+  let component: ChartBuilderComponent;
+  let fixture: ComponentFixture<ChartBuilderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ChartBuilderComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ChartBuilderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
